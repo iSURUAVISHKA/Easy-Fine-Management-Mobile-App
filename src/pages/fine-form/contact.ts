7b12f75d3444bf47d38d00085b31f357a470cee1
Isuru Avishka <isuruavishka@MacBook-Pro-2.local>
2017-05-09 09:05:11 +0530

    add data insertions

--- src/pages/fine-form/contact.ts
+++ src/pages/fine-form/contact.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HomeMenu } from '/Users/isuruavishka/finemgt/src/pages/home-menu/home-menu.ts';
+import {FirebaseListObservable, AngularFire} from 'angularfire2';
+
 
 @Component({
   selector: 'page-contact',
@@ -8,17 +10,46 @@ import { HomeMenu } from '/Users/isuruavishka/finemgt/src/pages/home-menu/home-m
 })
 export class ContactPage {
 
+  dbfine:FirebaseListObservable<any>;
   public userId : any;
   public amount : any;
+  public licenseNo : any;
+  public address : any;
+  public driverName: any;
   public fineNames : any;
   public points :any;
+  public phoneNo: any;
+  public policeStation: any;
+  public myDate: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public angFire:AngularFire) {
+
+      this.dbfine = angFire.database.list("/items");
+
       this.userId = localStorage.getItem("userId");
       this.amount = localStorage.getItem("amount");
       this.fineNames = localStorage.getItem("fineNames"); 
       this.points = localStorage.getItem("points");
       console.log(this.amount);
+
+      
+  }
+
+  addFine() {
+      console.log("Date"+this.myDate);
+    this.dbfine.push({
+      userId: this.userId,
+      amount: this.amount,
+      address: this.address,
+      name: this.driverName,
+      date: this.myDate,
+      licenseNo: this.licenseNo,
+      brokenlaw: this.fineNames,
+      penalitypoints: this.points,
+      contact: this.phoneNo,
+      policeStation: this.policeStation
+    });
+    this.navCtrl.pop(this);
   }
     goHome(){
     this.navCtrl.setRoot(HomeMenu);
